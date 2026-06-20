c7bd0f857240a93c35e39846696ba8357ebfd458
TankistPro <nakol.nayk@mail.ru>
2026-06-20 23:51:33 +0300

    feat: add guilds proto and generated TypeScript types

--- src/proto/paths.ts
+++ src/proto/paths.ts
@@ -3,4 +3,5 @@ import { join } from "path";
 export const PROTO_PATHS = {
   AUTH: join(__dirname, "../../proto/auth.proto"),
   USER: join(__dirname, "../../proto/user.proto"),
+  GUILDS: join(__dirname, "../../proto/guilds.proto"),
 } as const;
