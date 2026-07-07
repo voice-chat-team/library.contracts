c92ab2e7775cb8f294b71af1a08214fa8c7110df
TankistPro <nakol.nayk@mail.ru>
2026-07-07 15:33:11 +0300

    fix

--- src/proto/paths.ts
+++ src/proto/paths.ts
@@ -4,4 +4,5 @@ export const PROTO_PATHS = {
   AUTH: join(__dirname, "../../proto/auth.proto"),
   USER: join(__dirname, "../../proto/user.proto"),
   GUILDS: join(__dirname, "../../proto/guilds.proto"),
+  INVITATION: join(__dirname, "../../proto/invitation.proto"),
 } as const;
