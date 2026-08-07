dab924a54440efa864b0c7d637e625ee74bf0f58
TankistPro <nakol.nayk@mail.ru>
2026-08-08 00:46:35 +0300

    Add UPDATE_NOTIFICATION notification type

--- gen/notification.ts
+++ gen/notification.ts
@@ -14,6 +14,7 @@ export enum NotificationType {
   GUILD_MEMBER_ADD = 0,
   GUILD_MEMBER_LEAVE = 1,
   NEW_INVITE_TO_GUILD = 2,
+  UPDATE_NOTIFICATION = 3,
   UNRECOGNIZED = -1,
 }
 
--- src/constants/notification-type.constants.ts
+++ src/constants/notification-type.constants.ts
@@ -2,4 +2,5 @@ export const NotificationType = {
   GUILD_MEMBER_ADD: 0,
   GUILD_MEMBER_LEAVE: 1,
   NEW_INVITE_TO_GUILD: 2,
+  UPDATE_NOTIFICATION: 3,
 } as const;
